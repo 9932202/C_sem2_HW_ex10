673ef8337fc7cf72610d1a5e3e7ae22ab114ecb9
Анастасия Казарина <ana-kazarina@MacBook-Pro-Anastasia.local>
2023-01-04 13:00:14 +0300

    Решена задача 10

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿/* Задача 10: Напишите программу, которая принимает
+ на вход трёхзначное число и на выходе показывает 
+ вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1 */
+Console.Clear();
+Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number < 100 || number > 999){
+    Console.WriteLine($"Число {number} не является трехзначным");
+} // Проверка на трехзначность 
+
+int Divite(int number){
+    int result = number / 10;
+    return result;
+}
+
+int SecondNamber(int number){
+    int result = number % 10;
+    return result;
+}
+
+int div = Divite(number);
+int result = SecondNamber(div);
+
+Console.WriteLine($"вторая цифра числа: {result}");
